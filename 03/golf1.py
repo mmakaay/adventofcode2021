333d650f779201b8a66aef0b52da6ba4710e7e96
Maurice Makaay <account-github@makaay.nl>
2021-12-04 00:23:09 +0100

    Added the finished golf version for 3.1

--- 03/golf1.py
+++ 03/golf1.py
@@ -1,11 +1,5 @@
 #!/bin/env python3.10
-
 d=[map(int,l.strip())for l in open("input.txt")]
-g,e,t=0,0,len(d)/2
-c = list(zip(*d))
-for values in c:
-    add_gamma = sum(values) > t
-    g = (g << 1) | add_gamma
-    e = (e << 1) | (not add_gamma)
-
+g,e,t,c=0,0,len(d)/2,list(zip(*d))
+for v in c:a=sum(v)>t;g=g<<1|a;e=e<<1|(not a)
 print(g*e)
