39df608af7f349772b42c74a694050af8480c693
Maurice Makaay <account-github@makaay.nl>
2021-12-04 15:10:42 +0100

    Dropped 20 more bytes

--- 04-the-squid-game/golf1.py
+++ 04-the-squid-game/golf1.py
@@ -1,11 +1,9 @@
 #!/bin/env python3
 k,*l="".join(open("input.txt").readlines()).split("\n\n")
-n,b=list(map(int,k.split(","))),[list(map(int,"".join(y).split())) for y in l]
+e,n,b=1,list(map(int,k.split(","))),[list(map(int,"".join(y).split())) for y in l]
 for v in n:
  for w in b:
   if v in w:w[w.index(v)]=-1
   for i in range(5):
-   if all(f==-1 for f in w[i*5:i*5+5]) or all(f==-1 for f in w[i::5]):
-    print(sum(n for n in w if n>0)*v)
-    import sys
-    sys.exit()
+   if(all(f<0 for f in w[i*5:i*5+5])or all(f<0 for f in w[i::5]))and e:
+    print(sum(n for n in w if n>0)*v);e=0
