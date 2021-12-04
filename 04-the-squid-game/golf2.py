3b366eaf43efd096a68f38f2d6d0d6e6dc6a6b86
Maurice Makaay <account-github@makaay.nl>
2021-12-04 23:51:33 +0100

    Shaved off a few bytes

--- 04-the-squid-game/golf2.py
+++ 04-the-squid-game/golf2.py
@@ -1,11 +1,12 @@
 #!/bin/env python3
-def w(board):return any(all(f<0 for f in board[i*5:i*5+5])or all(f<0 for f in board[i::5])for i in range(5))
-k,*l="".join(open("input.txt")).split("\n\n")
-n,b=list(map(int,k.split(","))),[list(map(int,"".join(y).split()))for y in l]
+def w(b):return all(any(f>=0 for f in b[i*5:i*5+5])and any(f>=0 for f in b[i::5])for i in range(5))
+j="".join
+k,*l=j(open("input.txt")).split("\n\n")
+n,b=list(map(int,k.split(","))),[list(map(int,j(y).split()))for y in l]
 for v in n:
  for o in b:
   if v in o:o[o.index(v)]=-1
- if len(b)>1:b=[b for b in b if not w(b)]
- elif w(b[0]):break
+ if len(b)>1:b=list(filter(w,b))
+ elif not w(b[0]):break
 print(sum(n for n in b[0]if n>0)*v)
 
