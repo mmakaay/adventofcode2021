2362266af2da7485bec522fdb83f630213b419a0
Maurice Makaay <account-github@makaay.nl>
2021-12-05 13:53:15 +0100

    Sorry ... 8 bytes off golf1, 6 bytes off golf2

--- 05-vent-it-out/golf1.py
+++ 05-vent-it-out/golf1.py
@@ -1,8 +1,7 @@
 #!/bin/env python3
 import re
-q=list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())))
-m=max(q)+1;g,c=[0]*m*m,[q[i:i+4]for i in range(0,len(q),4)]
-for a,b,c,d in c:
+q=list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())));m=1000;g=[0]*m*m
+for a,b,c,d in [q[i:i+4]for i in range(0,len(q),4)]:
  if a==c or b==d:
   s=max(abs(a-c),abs(b-d));d,e=(c-a)//s,(d-b)//s
   for i in range(s+1):g[(b+i*e)*m+(a+i*d)]+=1
--- 05-vent-it-out/golf2.py
+++ 05-vent-it-out/golf2.py
@@ -1,8 +1,7 @@
 #!/bin/env python3
 import re
-o,q=max,list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())))
-m=o(q)+1;g,c=[0]*m*m,[q[i:i+4]for i in range(0,len(q),4)]
-for a,b,c,d in c:
+o,q=max,list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())));m=1000;g=[0]*m*m
+for a,b,c,d in [q[i:i+4]for i in range(0,len(q),4)]:
  s=o(abs(a-c),abs(b-d))
  for i in range(s+1):g[(b+i*(d-b)//s)*m+(a+i*(c-a)//s)]+=1
 print(sum(p>=2 for p in g))
