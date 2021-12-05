50fe5b5ee9df8beeedb0b5734d3fa87c442fea32
Maurice Makaay <account-github@makaay.nl>
2021-12-05 13:12:53 +0100

    Add puzzle 5 solution

---
+++ 05-vent-it-out/2.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+
+import re
+from itertools import chain
+
+# Read data.
+with open("input.txt") as f:
+  data = [list(map(int, re.split(',| -> ', line))) for line in f]
+
+# Initialize grid that can hold all data points.
+max_x = max(max(x1,x2) for x1,_,x2,_ in data)
+max_y = max(max(y1,y2) for y1,_,y2,_ in data)
+grid = [[0]*(max_x+1) for _ in range(max_y+1)]
+
+# Draw in all vent lines.
+for x1,y1,x2,y2 in data:
+  steps = max(abs(x1-x2), abs(y1-y2))
+  d_x = (x2-x1) // steps
+  d_y = (y2-y1) // steps
+  for i in range(steps+1):
+    x = x1 + i*d_x 
+    y = y1 + i*d_y 
+    grid[y][x] += 1
+
+print(sum(p>=2 for p in chain(*grid)))
---
+++ 05-vent-it-out/golf2.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+import re
+q=list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())))
+m,n=max(q[0::2])+1,max(q[1::2])+1
+g,c=[0]*m*n,[q[i:i+4]for i in range(0,len(q),4)]
+for a,b,c,d in c:
+ s=max(abs(a-c),abs(b-d))
+ d,e=(c-a)//s,(d-b)//s
+ for i in range(s+1):g[(b+i*e)*m+(a+i*d)]+=1
+print(sum(p>=2 for p in g))
