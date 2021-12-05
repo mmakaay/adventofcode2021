1d08014918fab6f2f64e1e836e160dedb537a5bf
Maurice Makaay <account-github@makaay.nl>
2021-12-05 13:29:32 +0100

    Shaving bytes

--- 05-vent-it-out/golf2.py
+++ 05-vent-it-out/golf2.py
@@ -1,10 +1,9 @@
 #!/bin/env python3
 import re
-q=list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())))
-m,n=max(q[0::2])+1,max(q[1::2])+1
-g,c=[0]*m*n,[q[i:i+4]for i in range(0,len(q),4)]
+o,q=max,list(map(int,re.split(',| -> |\n',"".join(open("input.txt")).strip())))
+m=o(q)+1
+g,c=[0]*m*m,[q[i:i+4]for i in range(0,len(q),4)]
 for a,b,c,d in c:
- s=max(abs(a-c),abs(b-d))
- d,e=(c-a)//s,(d-b)//s
- for i in range(s+1):g[(b+i*e)*m+(a+i*d)]+=1
+ s=o(abs(a-c),abs(b-d))
+ for i in range(s+1):g[(b+i*(d-b)//s)*m+(a+i*(c-a)//s)]+=1
 print(sum(p>=2 for p in g))
