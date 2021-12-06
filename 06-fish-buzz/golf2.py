2a1e0854b4907a37bb72a23b03519dd4d71d8b60
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-06 15:34:18 +0100

    Shave a few bytes off

--- 06-fish-buzz/golf2.py
+++ 06-fish-buzz/golf2.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
 d=[0]*10
 for i in map(int,next(open("input.txt")).split(",")):d[i+1]+=1
-for x in range(256):d=d[1:]+[0];d[9],d[7]=d[0],d[7]+d[0]
+for x in range(256):d=d[1:]+[d[1]];d[7]+=d[0]
 print(sum(d[1:]))
