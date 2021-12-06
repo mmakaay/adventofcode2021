1eea85f2fb5e873d4706cdacf52b9f179ff00ddc
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-06 14:07:16 +0100

    Improved golf solutions for 6

--- 06-fish-buzz/golf2.py
+++ 06-fish-buzz/golf2.py
@@ -1,7 +1,5 @@
 #!/bin/env python3
 d=[0]*10
 for i in map(int,next(open("input.txt")).split(",")):d[i+1]+=1
-for x in range(2304):
- c=x%9;d[c]=d[c+1]
- if c>7:d[0],d[9],d[7]=0,d[0],d[7]+d[0]
+for x in range(256):d=d[1:10]+[0];d[0],d[9],d[7]=0,d[0],d[7]+d[0]
 print(sum(d))
