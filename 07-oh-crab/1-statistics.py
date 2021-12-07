4d558d19708a6c7076d9923aa0a4ccf08aab779b
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-07 12:03:35 +0100

    Clean up

--- 07-oh-crab/1-statistics.py
+++ 07-oh-crab/1-statistics.py
@@ -2,14 +2,7 @@
 
 from statistics import median
 
-def read_crabs_from_file(path):
-  with open(path, "r") as f:
-    crabs = list(map(int, next(f).split(",")))
-  return crabs
-
-
-crabs = read_crabs_from_file("input.txt")
+crabs = list(map(int, next(open("input.txt")).split(",")))
 best_position = int(median(crabs))
 fuel = sum(abs(pos-best_position) for pos in crabs)
-
 print(fuel)
