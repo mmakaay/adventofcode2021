5fdee735ee710f7f534d8db66ea835492650cc81
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-09 15:09:58 +0100

    Added golf solution for 9b

--- 09-time-for-a-smoke/golf1.py
+++ 09-time-for-a-smoke/golf1.py
@@ -2,4 +2,3 @@
 def s(x,y):return[]+[(x,y-1)]*(y>0)+[(x-1,y)]*(x>0)+[(x+1,y)]*(x<w-1)+[(x,y+1)]*(y<h-1)
 g=[list(map(int,line.strip()))for line in open("input.txt")];w,h=len(g[0]),len(g)
 print(sum(g[b][a]+1 for b in range(h)for a in range(w)if all(g[y][x]>g[b][a]for x,y in s(a,b))))
-
---
+++ 09-time-for-a-smoke/golf2.py
@@ -0,0 +1,7 @@
+#!/bin/env python3
+M,E,H=map,len,range;G=[list(M(int,line.strip()))for line in open("input.txt")];w,o=E(G[0]),E(G)
+def W(J,E,M,I,G):return(J[G][I]<9)*(J[G][I]>J[M][E])
+def O(M,G):return[(M,G-1)]*(G>0)+[(M-1,G)]*(M>0)+[(M+1,G)]*(M<w-1)+[(M,G+1)]*(G<o-1)
+def T(B,A,H):H.add(A);[T(B,n,H)for n in O(*A)if W(B,*A,*n)];return H
+b=(T(G,H,set())for H in((a,b)for b in H(o)for a in H(w)if all(G[y][x]>G[b][a]for x,y in O(a,b))))
+q=list(M(E,sorted(b,key=E,reverse=1)[0:3]));print(q[0]*q[1]*q[2])
