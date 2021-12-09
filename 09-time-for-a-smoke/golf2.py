329c4b2cf406ac49223b32c6728d76bddcb5006c
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-09 15:43:02 +0100

    Scrape off a few more bytes

--- 09-time-for-a-smoke/golf2.py
+++ 09-time-for-a-smoke/golf2.py
@@ -4,4 +4,4 @@ def W(J,E,M,I,G):return(J[G][I]<9)*(J[G][I]>J[M][E])
 def O(M,G):return[(M,G-1)]*(G>0)+[(M-1,G)]*(M>0)+[(M+1,G)]*(M<w-1)+[(M,G+1)]*(G<o-1)
 def T(B,A,H):H.add(A);[T(B,n,H)for n in O(*A)if W(B,*A,*n)];return H
 b=(T(G,H,set())for H in((a,b)for b in H(o)for a in H(w)if all(G[y][x]>G[b][a]for x,y in O(a,b))))
-q=list(M(E,sorted(b,key=E,reverse=1)[0:3]));print(q[0]*q[1]*q[2])
+q=sorted(b,key=E,reverse=1);print(E(q[0])*E(q[1])*E(q[2]))
