c660dcd5a5cf7125e34e546cd3d1a45d105b793a
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-10 14:57:04 +0100

    Hmm

--- 10-unbalanced/golf2.py
+++ 10-unbalanced/golf2.py
@@ -3,5 +3,5 @@ M,E,H="([{<",")]}>",[]
 for l in open("input.txt"):
  s,o,S="",0,0
  for c in l.strip():o+=s==(s:=E[M.index(c)]+s if c in M else s[s[0]==c:])
- if o<1:[S:=1+S*5+E.index(c)for c in s];H=sorted(H+[S])
+ H=sorted(H+[S:=1+S*5+E.index(c)for c in s][-1:]*(o<1))
 print(H[len(H)//2])
