7ad6f1bce85f83eeb5b84f68b3c9aedf6f03be69
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-10 14:43:54 +0100

    Went overboard with scraping bytes

--- 10-unbalanced/golf2.py
+++ 10-unbalanced/golf2.py
@@ -2,9 +2,6 @@
 M,E,H="([{<",")]}>",[]
 for l in open("input.txt"):
  s,o,S="",0,0
- for c in l.strip():
-  if c in M:s=E[M.index(c)]+s
-  elif s[0]!=c:o+=1
-  else:s=s[1:]
+ for c in l.strip():o+=s==(s:=E[M.index(c)]+s if c in M else s[s[0]==c:])
  if o<1:[S:=1+S*5+E.index(c)for c in s];H=sorted(H+[S])
 print(H[len(H)//2])
