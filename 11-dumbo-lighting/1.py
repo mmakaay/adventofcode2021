fc2ebc8ad822daf78c8a46ce2852ac8ede9d2b37
Maurice Makaay <account-github@makaay.nl>
2021-12-11 17:57:02 +0100

    As a little experiment, made 11a more functional

--- 11-dumbo-lighting/1.py
+++ 11-dumbo-lighting/1.py
@@ -1,5 +1,8 @@
 #!/bin/env python3
 
+from functools import reduce
+
+
 def load_octopi(path):
   with open(path) as f:
     return [list(map(int, line.strip())) for line in f]
@@ -39,30 +42,34 @@ def positions(grid):
 
 
 def within_bounds(grid, positions):
-  return (
+  return list(
     (x, y) for x, y in positions if 
     x >= 0 and y >= 0 and x < grid_width(grid) and y < grid_height(grid)
   )
 
 
-def increase_energy_level_at_pos(octopi, pos):
-  x,y = pos
-  octopi[y][x] += 1
+def increase_energy_level_at(octopi, at_positions):
+  return [
+    [
+      value+1 if (x,y) in at_positions else value
+      for x,value in enumerate(row)
+    ]
+    for y,row in enumerate(octopi)
+  ]
 
 
-def increase_energy_level(octopi):
-  for pos in positions(octopi):
-    increase_energy_level_at_pos(octopi, pos)
+def increase_all_energy_levels(octopi):
+  return increase_energy_level_at(octopi, positions(octopi))
 
 
-def deplete_energy_at_pos(octopi, pos):
-  x,y = pos
-  octopi[y][x] = 0
-
-
-def deplete_energy_at_positions(octopi, positions):
-  for pos in positions:
-    deplete_energy_at_pos(octopi, pos)
+def deplete_energy_level_at(octopi, at_positions):
+  return [
+    [
+      0 if (x,y) in at_positions else value
+      for x,value in enumerate(row)
+    ]
+    for y,row in enumerate(octopi)
+  ]
 
 
 def find_flash_positions(octopi): 
@@ -78,23 +85,23 @@ def flash(octopi, flashed=[]):
   )
   if new_flash_positions:
     for flash_pos in new_flash_positions:
-      for around in within_bounds(octopi, get_surrounding(flash_pos)):
-        increase_energy_level_at_pos(octopi, around)
+      surrounding = within_bounds(octopi, get_surrounding(flash_pos))
+      octopi = increase_energy_level_at(octopi, surrounding)
     return flash(octopi, flashed + new_flash_positions)
-  return flashed
+  return octopi, flashed
 
 
-def cycle_octopi(octopi):
-  increase_energy_level(octopi)
-  flashed = flash(octopi)
-  deplete_energy_at_positions(octopi, flashed)
-  return flashed
+def cycle_octopi(octopi, flashed=[]):
+  octopi = increase_all_energy_levels(octopi)
+  octopi, new_flashed = flash(octopi)
+  octopi = deplete_energy_level_at(octopi, new_flashed)
+  return octopi, flashed + new_flashed
 
 
 octopi = load_octopi("input.txt") 
-all_flashes = []
+number_of_flashes = 0
 for _ in range(100):
-  flashed = cycle_octopi(octopi)
-  all_flashes += flashed
+  octopi, flashed = cycle_octopi(octopi)
+  number_of_flashes += len(flashed)
 
-print(len(all_flashes))
+print(number_of_flashes)
