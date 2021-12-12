52fd052410519f67be8be282edc8c7c9de94a601
Maurice Makaay <account-github@makaay.nl>
2021-12-12 11:07:02 +0100

    Hey, some more bytes gone!

--- 11-dumbo-lighting/golf1.py
+++ 11-dumbo-lighting/golf1.py
@@ -5,6 +5,6 @@ for n in range(100):
  f,G=[],[chr(o(x)+1)for x in G]
  while X:=[j for j,x in enumerate(G)if o(x)>57 and j not in f]:
   for n in X:
-   A,G[n],f,a,b,c,d=A+1,'0',f+[n],n%H>0,(n+1)%H>0,n//H>0,n//H<R-1;r=[p for p in[n-1]*a+[n-H-1]*a*c+[n+H-1]*a*d+[n+1]*b+[n-H]*c+[n-H+1]*c*b+[n+H]*d+[n+H+1]*d*b]
+   A,G[n],f,a,b=A+1,'0',f+[n],n%H>0,(n+1)%H>0;r=[p for p in[n-1]*a+[n-H-1]*a+[n+H-1]*a+[n+1]*b+[n-H+1]*b+[n+H+1]*b+[n-H,n+H]if H*R>p>=0]
    for p in r:G[p]=chr(o(G[p])+1*(p not in f))
 print(A)
