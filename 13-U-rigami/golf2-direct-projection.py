98bca8eb8cafaf51f8d7fd0359bd9f05bdb177f9
Maurice Makaay <account-github@makaay.nl>
2021-12-14 00:33:51 +0100

    SHAVE MORE BYTES, MOOORRRREgit diffgit diff

--- 13-U-rigami/golf2-direct-projection.py
+++ 13-U-rigami/golf2-direct-projection.py
@@ -2,6 +2,6 @@
 V,P=list(open("input.txt")),["","2J","H"]
 for x,y in[map(int,l.split(","))for l in V if","in l]:
  for a,o in[(l[11]=="y",int(l[13:]))for l in V if"="in l]:
-  x,y=2*o-x if a<1 and x>o else x,2*o-y if a and y>o else y
+  x,y=[2*o-x,x][(x<o)|a],[y,2*o-y][a*y>o]
  P+=[f"{y+1};{x+1}f*"]
 print("\033[".join(P+["7;0f"]))
