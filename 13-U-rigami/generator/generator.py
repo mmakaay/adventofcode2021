44b18b3539838e7b99cfb66cd67a8fbf4cbb5be9
Maurice Makaay <account-github@makaay.nl>
2021-12-14 22:10:14 +0100

    Finished generator

--- 13-U-rigami/generator/generator.py
+++ 13-U-rigami/generator/generator.py
@@ -6,13 +6,13 @@ import sys
 dots = set()
 with open(sys.argv[1], "r") as f:
   for y,line in enumerate(f):
-    for x, char in enumerate(line):
+   for x, char in enumerate(line):
       if char=="*":
         dots.add((x,y)) 
     
 folds = []
-for _ in range(5):
-  axis = choice(["y", "x"])
+for _ in range(10):
+  axis = choice(["x", "y"])
   w = max(x for x,_ in dots)+1
   h = max(y for _,y in dots)+1
   offset = w if axis == "x" else h
@@ -22,17 +22,17 @@ for _ in range(5):
     if axis == "x":
         if x > (offset-size): 
           dots.add((2*offset-x,y))
-          if choice([True, False]):
+          if choice([True, True]):
             dots.remove((x,y))
     else:
         if y > (offset-size): 
           dots.add((x,2*offset-y))
-          if choice([True, False]):
+          if choice([True, True]):
             dots.remove((x,y))
   folds.append((axis,offset))
 
 for x,y in dots:
   print(f"{x},{y}")
 print()
-for axis,size in folds:
+for axis,size in reversed(folds):
   print(f"fold along {axis}={size}")
--- 13-U-rigami/generator/test.py
+++ 13-U-rigami/generator/test.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
-B,V,P=0,list(open("input.txt")),["","2J","H"]
+import sys;B,V,P=0,list(open(sys.argv[1])),["","2J","H"]
 for x,y in[map(int,l.split(","))for l in V if","in l]:
  for a,o in[(l[11]=="y",int(l[13:]))for l in V if"="in l]:
   x,y=[2*o-x,x][(x<o)|a],[y,2*o-y][a*y>o]
