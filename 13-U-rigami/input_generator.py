34cdc850afcdbe2ba51f32f5f910e94d3dca8dac
Maurice Makaay <account-github@makaay.nl>
2021-12-14 01:57:09 +0100

    Working on a generator

---
+++ 13-U-rigami/input_generator.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+from random import choice, randrange, sample
+
+import sys
+dots = set()
+with open(sys.argv[1], "r") as f:
+  for y,line in enumerate(f):
+    for x, char in enumerate(line):
+      if char=="*":
+        dots.add((x,y)) 
+    
+folds = []
+for _ in range(2):
+  axis = choice(["y", "x"])
+  w = max(x for x,_ in dots)
+  h = max(y for _,y in dots)
+  offset = w if axis == "x" else h
+  size = randrange(offset)
+  grab = sample(dots, len(dots)//2)
+  for x,y in grab:
+    if axis == "x":
+        if x >= (offset-size): 
+          dots.add((2*offset-x,y))
+    else:
+        if y >= (offset-size): 
+          dots.add((x,2*offset-y))
+    if choice([True, False]):
+      dots.remove((x,y))
+  folds.append((axis,offset))
+
+for x,y in dots:
+  print(f"{x},{y}")
+print()
+for axis,size in folds:
+  print(f"fold along {axis}={size}")
