7becdde93f5670d48f9c16b019df3f1c2524189d
Maurice Makaay <account-github@makaay.nl>
2021-12-16 01:02:22 +0100

    Dropped a whopping 29 bytes

--- 15-tons-of-chit/golf2.py
+++ 15-tons-of-chit/golf2.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 from heapq import*;C=[list(map(int,line.strip()))for line in open("input.txt")]
-o,p,V,R,D=len(C[0]),len(C),[(0,0,0)],{},set();w,h=o*5,p*5;U=[[0]*w for _ in range(h)]
-for i in range(h*w):f,g=i//w,i%w;U[f][g]=1+(C[f%p][g%o]+g//o+f//p-1)%9 
+o,p,V,R,D=len(C[0]),len(C),[(0,0,0)],{},set();w,h=o*5,p*5
+U=[[1+(C[y%p][x%o]+x//o+y//p-1)%9 for x in range(w)]for y in range(h)]
 while V:
  r,x,y=heappop(V);D.add((x,y))
  for N in[x for x in [(x-1,y)]*(x>0)+[(x,y-1)]*(y>0)+[(x+1,y)]*(x<(w-1))+[(x,y+1)]*(y<(h-1))if x not in D]:
