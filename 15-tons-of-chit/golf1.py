d9313922e61999ff9317c327a006db8a7f3c753d
Maurice Makaay <account-github@makaay.nl>
2021-12-16 01:56:58 +0100

    Last shave

--- 15-tons-of-chit/golf1.py
+++ 15-tons-of-chit/golf1.py
@@ -4,7 +4,7 @@ C=[list(map(int,line.strip()))for line in open("input.txt")]
 w,h,V,R,D=len(C[0])-1,len(C)-1,[(0,0,0)],{},set()
 while V:
  r,x,y=heappop(V);D.add((x,y))
- for N in [x for x in [(x-1,y)]*x+[(x,y-1)]*y+[(x+1,y)]*(x<w)+[(x,y+1)]*(y<h) if x not in D]:
+ for N in[x for x in[(x-1,y)]*x+[(x,y-1)]*y+[(x+1,y)]*(x<w)+[(x,y+1)]*(y<h)if x not in D]:
   k,l=N;u,y=C[l][k],R.get(N,2**99);t=r+u
   if t<y:R[N]=t;heappush(V,(t,k,l))
 print(R[h,w])
--- 15-tons-of-chit/golf2.py
+++ 15-tons-of-chit/golf2.py
@@ -4,7 +4,7 @@ o,p,V,R,D=len(C[0]),len(C),[(0,0,0)],{},set();w,h=o*5,p*5
 U=[[1+(C[y%p][x%o]+x//o+y//p-1)%9 for x in range(w)]for y in range(h)]
 while V:
  r,x,y=heappop(V);D.add((x,y))
- for N in[x for x in [(x-1,y)]*(x>0)+[(x,y-1)]*(y>0)+[(x+1,y)]*(x<(w-1))+[(x,y+1)]*(y<(h-1))if x not in D]:
+ for N in[x for x in[(x-1,y)]*(x>0)+[(x,y-1)]*(y>0)+[(x+1,y)]*(x<(w-1))+[(x,y+1)]*(y<(h-1))if x not in D]:
   k,l=N;u,y=U[l][k],R.get(N,2**99);t=r+u
   if t<y:R[N]=t;heappush(V,(t,k,l))
 print(R[h-1,w-1])
