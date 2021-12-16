edbf464649e462828bfe5bedeea0465c26d3a881
Maurice Makaay <mmakaay1@xs4all.net>
2021-12-16 18:09:23 +0100

    Shaving

--- 16-bitte-ein-bit/golf1.py
+++ 16-bitte-ein-bit/golf1.py
@@ -4,14 +4,14 @@ def Z():
  V,T,L,M,O,P=I(3),I(3),6,1,0,0
  if T==4:
   while M:M=B();P=P<<4|I(4);O+=5
-  return L+O,(V,T,P)
+  return(V,T,P),L+O
  O,P=X(B())
- return L+M+O,(V,T,P)
+ return(V,T,P),L+M+O
 def X(D):
  O=15-4*D;W,A=I(O),[]
- while W:P,V=Z();O+=P;A.append(V);W-=1 if D else P
+ while W:V,P=Z();O+=P;A.append(V);W-=[P,1][D]
  return O,A
 def V(t,T=0):v,t,a=t;return T+(sum(map(V,a))if t!=4 else 0)+v
 N=[map(int,f"{int(h,16):04b}")for h in next(open("input.txt")).strip().upper()]
 B=lambda:next(b for n in N for b in n)
-print(V(Z()[1]))
+print(V(Z()[0]))
--- 16-bitte-ein-bit/golf2.py
+++ 16-bitte-ein-bit/golf2.py
@@ -3,20 +3,20 @@ from operator import *
 from functools import reduce as r
 def I(l,v=0):return I(l-1,v<<1|B())if l else v
 def Z():
- V,T,L,M,O,P=I(3),I(3),6,1,0,0
+ I(3);T,L,M,O,P=I(3),6,1,0,0
  if T==4:
   while M:M=B();P=P<<4|I(4);O+=5
-  return L+O,(V,T,P)
+  return(T,P),L+O
  O,P=X(B())
- return L+M+O,(V,T,P)
+ return(T,P),L+M+O
 def X(D):
  O=15-4*D;W,A=I(O),[]
- while W:P,V=Z();O+=P;A.append(V);W-=1 if D else P
+ while W:V,P=Z();O+=P;A.append(V);W-=[P,1][D]
  return O,A
 def V(T):
- _,t,a=T;f=[add,mul,min,max,0,gt,lt,eq][t]
+ t,a=T;f=[add,mul,min,max,0,gt,lt,eq][t]
  if f:a=int(r(f,[V(x)for x in a]))
  return a
 N=[map(int,f"{int(h,16):04b}")for h in next(open("input.txt")).strip().upper()]
 B=lambda:next(b for n in N for b in n)
-print(V(Z()[1]))
+print(V(Z()[0]))
