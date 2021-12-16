99ab29bf72eb2b1acbe87785d33785ff824624b1
Maurice Makaay <account-github@makaay.nl>
2021-12-17 00:39:14 +0100

    Even Morrrrr

--- 16-bitte-ein-bit/golf1.py
+++ 16-bitte-ein-bit/golf1.py
@@ -1,16 +1,16 @@
 #!/bin/env python3
 def I(l,v=0):return I(l-1,v<<1|B())if l else v
 def Z():
- V,T,L,M,O,P=I(3),I(3),6,1,0,0
- if T==4:
-  while M:M=I(5)&16;O+=5
- else:O,P=X(B())
- return(V,T,P),L+M+O
+ G,A,T,V,E,R=I(3),I(3),6,1,0,0
+ if A==4:
+  while V:V=I(5)&16;E+=5
+ else:E,R=X(B())
+ return(G,A,R),T+V+E
 def X(D):
- O=15-4*D;W,A=I(O),[]
- while W:V,P=Z();O+=P;A.append(V);W-=[P,1][D]
- return O,A
-def V(t,T=0):v,t,a=t;return T+(sum(map(V,a))if t!=4 else 0)+v
+ E=15-4*D;W,A=I(E),[]
+ while W:G,R=Z();E+=R;A+=[G];W-=[R,1][D]
+ return E,A
+def G(t,A=0):v,t,a=t;return A+(sum(map(G,a))if t!=4 else 0)+v
 i=(b=="1"for n in(f"{int(h,16):04b}"for h in
 next(open("input.txt")).strip())for b in n)
-B=lambda:next(i);print(V(Z()[0]))
+B=lambda:next(i);print(G(Z()[0]))
--- 16-bitte-ein-bit/golf2.py
+++ 16-bitte-ein-bit/golf2.py
@@ -1,20 +1,20 @@
 #!/bin/env python3
 from operator import *
-from functools import reduce as r
+from functools import *
 def I(l,v=0):return I(l-1,v<<1|B())if l else v
 def Z():
- I(3);T,L,M,O,P=I(3),6,1,0,0
+ T,L,M,O,P=I(6)&7,6,1,0,0
  if T==4:
   while M:M=B();P=P<<4|I(4);O+=5
  else:O,P=X(B())
  return(T,P),L+M+O
 def X(D):
  O=15-4*D;W,A=I(O),[]
- while W:V,P=Z();O+=P;A.append(V);W-=[P,1][D]
+ while W:V,P=Z();O+=P;A+=[V];W-=[P,1][D]
  return O,A
 def V(T):
  t,a=T;f=[add,mul,min,max,0,gt,lt,eq][t]
- if f:a=int(r(f,map(V,a)))
+ if f:a=int(reduce(f,map(V,a)))
  return a
 i=(b=="1"for n in(f"{int(h,16):04b}"for h in
 next(open("input.txt")).strip())for b in n)
