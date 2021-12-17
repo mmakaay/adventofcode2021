7fb2c5f987f42b380cb907e37a793bbc9fd4005f
Maurice Makaay <account-github@makaay.nl>
2021-12-17 10:53:38 +0100

    Like I tend to say: het kan altijd kleiner

--- 16-bitte-ein-bit/golf1.py
+++ 16-bitte-ein-bit/golf1.py
@@ -1,14 +1,14 @@
 #!/bin/env python3
 def I(l,v=0):return I(l-1,v<<1|B())if l else v
 def Z():
- G,A,T,V,E,R=I(3),I(3),6,1,0,0
+ G,A,V,E,R=I(3),I(3),1,0,0
  if A-4:
-  D=B();E=15-4*D;W,R=I(E),[]
-  while W:H,J=Z();E+=J;R+=[H];W-=[J,1][D]
+  D=B();E=15-4*D;W=I(E)
+  while W:J,t=Z();G+=t;E+=J;W-=[J,1][D]
  else:
   while V:V=I(5)&16;E+=5
- return(G,A,R),T+V+E
-def G(t,A=0):v,t,a=t;return A+(sum(map(G,a))if t!=4 else 0)+v
+ return 6+V+E,G
 i=(b=="1"for n in(f"{int(h,16):04b}"for h in
 next(open("input.txt")).strip())for b in n)
-B=lambda:next(i);print(G(Z()[0]))
+B=lambda:next(i)
+print(Z()[1])
--- 16-bitte-ein-bit/golf2.py
+++ 16-bitte-ein-bit/golf2.py
@@ -3,16 +3,16 @@ from operator import *
 from functools import *
 def I(l,v=0):return I(l-1,v<<1|B())if l else v
 def Z():
- T,L,M,O,P=I(6)&7,6,1,0,0
+ T,M,O,P=I(6)&7,1,0,0
  if T-4:
   D=B();O=15-4*D;W,P=I(O),[]
   while W:R,Q=Z();O+=Q;P+=[R];W-=[Q,1][D]
  else:
   while M:M=B();P=P<<4|I(4);O+=5
- return(T,P),L+M+O
+ return([add,mul,min,max,0,gt,lt,eq][T],P),6+M+O
 def V(T):
- t,a=T;f=[add,mul,min,max,0,gt,lt,eq][t]
- if f:a=int(reduce(f,map(V,a)))
+ f,a=T
+ if f:a=reduce(f,map(V,a))
  return a
 i=(b=="1"for n in(f"{int(h,16):04b}"for h in
 next(open("input.txt")).strip())for b in n)
