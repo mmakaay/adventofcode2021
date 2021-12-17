d3667c5e36be901481d6a5124ceb63dcb8b5e68b
Maurice Makaay <account-github@makaay.nl>
2021-12-17 11:01:52 +0100

    Noggit add .

--- 16-bitte-ein-bit/golf2.py
+++ 16-bitte-ein-bit/golf2.py
@@ -3,17 +3,14 @@ from operator import *
 from functools import *
 def I(l,v=0):return I(l-1,v<<1|B())if l else v
 def Z():
- T,M,O,P=I(6)&7,1,0,0
+ T,M,O,P=I(6)&7,1,0,[0]
  if T-4:
   D=B();O=15-4*D;W,P=I(O),[]
   while W:R,Q=Z();O+=Q;P+=[R];W-=[Q,1][D]
  else:
-  while M:M=B();P=P<<4|I(4);O+=5
- return([add,mul,min,max,0,gt,lt,eq][T],P),6+M+O
-def V(T):
- f,a=T
- if f:a=reduce(f,map(V,a))
- return a
+  while M:M=B();P=[P[0]<<4|I(4)];O+=5
+ return(reduce([add,mul,min,max,lambda x:x,gt,lt,eq][T],P)),6+M+O
 i=(b=="1"for n in(f"{int(h,16):04b}"for h in
 next(open("input.txt")).strip())for b in n)
-B=lambda:next(i);print(V(Z()[0]))
+B=lambda:next(i)
+print(Z()[0])
