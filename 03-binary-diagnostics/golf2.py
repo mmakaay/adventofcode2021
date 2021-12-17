b7b6cca7a048d0321c2ba68d8aed87a91a0f93b1
Maurice Makaay <account-github@makaay.nl>
2021-12-17 22:25:36 +0100

    Some lingering changes that were lying around

--- 03-binary-diagnostics/golf2.py
+++ 03-binary-diagnostics/golf2.py
@@ -1,8 +1,8 @@
 #!/bin/env python3.10
 def s(d,u,c=0):
- if len(d)==1:return d[0]
- t=len(d)/2;y=sum(line[c]for line in d)>=t;b=y if u else not y
+ if len(d)<2:return d[0]
+ t=len(d)/2;y=sum(line[c]for line in d)>=t;b=[not y,y][u]
  return s(list(filter(lambda r:r[c]==b,d)),u,(c+1)%len(d[0]))
 i=lambda x:int("".join(map(str,x)),2)
 d=[list(map(int,line.strip()))for line in open("input.txt")]
-print(i(s(d,True))*i(s(d,False)))
+print(i(s(d,1))*i(s(d,0)))
