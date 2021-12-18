1b31256d59aa9ab36b90142e48c4f2cef9f1d0e2
Maurice Makaay <account-github@makaay.nl>
2021-12-19 00:04:20 +0100

    Got parsing, exploding, splitting and reducing working

--- 18-hit-the-snail-on-the-head/tests.py
+++ 18-hit-the-snail-on-the-head/tests.py
@@ -122,7 +122,6 @@ class TestSnailParser(unittest.TestCase):
                 self.assertEqual(code, str(root))
 
     def test_parser_error_cases(self):
-        parser = SnailParser()
         for code,error_pos, expected_in_msg in [
             ("", 0, "Unexpected end"),
             ("[", 1, "Unexpected end"),
@@ -141,8 +140,135 @@ class TestSnailParser(unittest.TestCase):
         ]:
             with self.subTest():
                 with self.assertRaises(SnailParserException) as context:
-                    parser.parse(code)
+                    parse_snail_code(code)
                 self.assertEqual(error_pos, context.exception.pos)
                 self.assertIn(expected_in_msg, str(context.exception))
 
+    def test_parse_snail_code_function(self):
+        root = parse_snail_code("[1,[2,[3,[4,[5,6]]]]]")
+        self.assertEqual("[1,[2,[3,[4,[5,6]]]]]", str(root))
+
+
+class TestSnailExploder(unittest.TestCase):
+    def test_explode_not(self):
+        root = parse_snail_code("[9,9]")
+        exploded = SnailExploder(root).run()
+        self.assertFalse(exploded)
+        self.assertEqual("[9,9]", str(root))
+        
+    def test_explode_one_to_the_left(self):
+        root = parse_snail_code("[1,[2,[3,[4,[5,6]]]]]")
+        exploded = SnailExploder(root).run()
+        self.assertTrue(exploded)
+        self.assertEqual("[1,[2,[3,[9,0]]]]", str(root))
+
+    def test_explode_one_to_the_right(self):
+        root = parse_snail_code("[[[[[1,2],3],4],5],6]")
+        SnailExploder(root).run()
+        self.assertEqual("[[[[0,5],4],5],6]", str(root))
+
+    def test_explode_one_two_sides(self):
+        root = parse_snail_code("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]")
+        SnailExploder(root).run()
+        self.assertEqual("[[[[0,7],4],[15,[0,13]]],[1,1]]", str(root))
+
+    def test_explode_cases_from_assignment(self):
+        for code, exploded in [
+            ("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"),
+            ("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[15,[0,13]]],[1,1]]"),
+            ("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"), 
+            ("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+            ("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+            ("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+            ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+            ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
+        ]:
+            with self.subTest():
+                root = parse_snail_code(code)
+                SnailExploder(root).run()
+                self.assertEqual(exploded, str(root))
+
+
+class TestSnailSplitter(unittest.TestCase):
+    def test_split_not(self):
+        root = parse_snail_code("[9,9]")
+        splitted = SnailSplitter(root).run()
+        self.assertFalse(splitted)
+        self.assertEqual("[9,9]", str(root))
+
+    def test_split_simple_even_left(self):
+        root = parse_snail_code("[10,9]")
+        splitted = SnailSplitter(root).run()
+        self.assertTrue(splitted)
+        self.assertEqual("[[5,5],9]", str(root))
+
+    def test_split_simple_uneven_left(self):
+        root = parse_snail_code("[11,9]")
+        SnailSplitter(root).run()
+        self.assertEqual("[[5,6],9]", str(root))
+
+    def test_split_simple_only_once(self):
+        root = parse_snail_code("[[9,21],[22,23]]")
+        SnailSplitter(root).run()
+        self.assertEqual("[[9,[10,11]],[22,23]]", str(root))
+
+    def test_split_cases_from_assignment(self):
+        for code, splitted in [
+            ("[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"),
+            ("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"),
+        ]:
+            with self.subTest():
+                root = parse_snail_code(code)
+                SnailSplitter(root).run()
+                self.assertEqual(splitted, str(root))
+
+
+class TestSnailReduce(unittest.TestCase):
+    def test_reduce_not(self):
+        root = parse_snail_code("[9,9]")
+        reduced = root.reduce()
+        self.assertFalse(reduced)
+        self.assertEqual("[9,9]", str(root))
+
+    def test_reduce_one_explode(self):
+        root = parse_snail_code("[[[[[1,2],3],4],5],6]")
+        reduced = root.reduce()
+        self.assertTrue(reduced)
+        self.assertEqual("[[[[0,5],4],5],6]", str(root))
+
+    def test_reduce_two_explodes(self):
+        root = parse_snail_code("[[[[[1,2],[3,4]],5],6],7]")
+        reduced = root.reduce()
+        self.assertTrue(reduced)
+        self.assertEqual("[[[[5,0],9],6],7]", str(root))
+
+    def test_reduce_one_split(self):
+        root = parse_snail_code("[10,1]")
+        reduced = root.reduce()
+        self.assertTrue(reduced)
+        self.assertEqual("[[5,5],1]", str(root))
+
+    def test_reduce_two_splits(self):
+        root = parse_snail_code("[10,10]")
+        reduced = root.reduce()
+        self.assertTrue(reduced)
+        self.assertEqual("[[5,5],[5,5]]", str(root))
+
+    def test_reduce_buttload(self):
+        root = parse_snail_code("[[[[[1,6],[6,7]],8],[9,10]],11]")
+        reduced = root.reduce()
+        self.assertTrue(reduced)
+        self.assertEqual("[[[[0,6],[7,8]],[9,[5,5]]],[5,6]]", str(root))
+        [[[[0,[9,9]],8],7],6]
+
+
+
+class TestSnailReduce(unittest.TestCase):
+    def test_magnitude_simple(self):
+        root = parse_snail_code("[1,2]")
+        magnitude = SnailMagnitudeComputer(root).run()
+        self.assertEqual((3*1 + 2*2), magnitude)
+
+
 unittest.main()
+
