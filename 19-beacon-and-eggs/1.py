1303203bb52fcc8ccfc3573039394915eca3f9a8
Maurice Makaay <account-github@makaay.nl>
2021-12-21 12:03:35 +0100

    WIP

--- 19-beacon-and-eggs/1.py
+++ 19-beacon-and-eggs/1.py
@@ -18,51 +18,35 @@ def load_scanners(path):
                 scanner.append(beacon)
     return scanners            
 
-twist_0 = lambda x, y, z: (x, y, z)
-twist_90 = lambda x, y, z: (-y, x, z)
-twist_180 = lambda x, y, z: (-x, -y, z)
-twist_270 = lambda x, y, z: (y, -x, z)
-twists = [twist_0, twist_90, twist_180, twist_270]
 
-rot_0 = lambda x, y, z: (x, y, z)
-rot_90Yccw = lambda x, y, z: (-z, y, x)
-rot_90Ycw = lambda x, y, z: (z, y, -x)
-rot_90Xccw = lambda x, y, z: (x, -z, y)
-rot_90Xcw = lambda x, y, z: (x, z, -y)
-rot_180 = lambda x, y, z: (-x, y, -z)
-rotations = [rot_0, rot_90Yccw, rot_90Ycw, rot_90Xccw, rot_90Xcw, rot_180]
+def make_transformations():
+    def twists():
+        yield lambda x, y, z: (x, y, z)
+        yield lambda x, y, z: (-y, x, z)
+        yield lambda x, y, z: (-x, -y, z)
+        yield lambda x, y, z: (y, -x, z)
+    def rotations():
+        yield lambda x, y, z: (x, y, z)
+        yield lambda x, y, z: (-z, y, x)
+        yield lambda x, y, z: (z, y, -x)
+        yield lambda x, y, z: (x, -z, y)
+        yield lambda x, y, z: (x, z, -y)
+        yield lambda x, y, z: (-x, y, -z)
+    def make_transformation(twist, rotate):
+        return lambda pos: twist(*rotate(*pos))
+    return [
+        make_transformation(twist, rotate)
+        for twist, rotate in product(twists(), rotations())
+    ]
 
-def make_transformation(twist, rotate):
-    return lambda pos: twist(*rotate(*pos))
 
-transformations = [
-    make_transformation(twist, rotate)
-    for twist, rotate in product(twists, rotations)
-]
+def apply_transformations(beacons, transformations):
+    for transformation in transformations:
+        yield list(map(transformation, beacons))
 
-def generate_transformations(beacons):
-    for transform in transformations:
-        yield list(map(transform, beacons))
 
 def taxi_distance(a, b):
     return sum(abs(p1-p2) for p1,p2 in zip(a,b))
-
-
-#def fingerprint(beacons, depth):
-#    fingerprints = set()
-#    for beacon in beacons:
-#        o = sorted(taxi_distance(beacon, other) for other in beacons)
-#        fingerprints.add((tuple(o[0:depth]), beacon))
-#    return fingerprints
-#        
-    #beacon_distances = [(beacon, taxi_distance((0,0,0),beacon)) for beacon in beacons]
-    #beacons_by_distance = [beacon for beacon,_ in sorted(beacon_distances, key=lambda x:x[1])]
-    #fingerprints = set()
-#
-#    for i in range(len(beacons)-1):
-#        pair = beacons[i:i+2]
-#        fingerprints.add(taxi_distance(*pair))
-#    return fingerprints
 
 
 if len(argv) != 2:
@@ -72,8 +56,9 @@ if len(argv) != 2:
 scanners = load_scanners(argv[1])
 stitched = scanners.pop()
 beacons = scanners[0]
+transformations = make_transformations()
 for beacons in scanners:
-    for rotated in generate_transformations(beacons):
+    for rotated in apply_transformations(beacons, transformations):
         print(rotated[0])
    
 
